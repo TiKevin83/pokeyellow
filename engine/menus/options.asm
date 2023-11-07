110a3c927e2fb329c97c9ff262850cf14dc4f6c0
TiKevin83 <travismcgeehan@gmail.com>
2023-11-07 00:28:49 -0500

    Actually fix fast options

--- engine/menus/options.asm
+++ engine/menus/options.asm
@@ -1,9 +1,11 @@
 DisplayOptionMenu_:
-	; This line fixes the Fast Options glitch
+	; This section fixes the Fast Options glitch
 	; The code as written applies any direction button presses done simultaneous with selecting the menu
 	; To each option while rendering the menu
 IF DEF(_BUGFIX)
 	call JoypadLowSensitivity
+	xor a
+	ldh [hJoy5], a
 ENDC
 	call InitOptionsMenu
 .optionMenuLoop
